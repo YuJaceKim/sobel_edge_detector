d7b84a9f652d13ec34c9af604c99dff089f82b25
Jace <jace@jaceui-macbook-pro.local>
2018-11-29 14:06:32 +0900

    Initial Commit

---
+++ sobel_edge_detector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sobel_edge_detector
+//
+//  Created by Jace on 2018. 11. 29..
+//  Copyright © 2018년 Jace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
